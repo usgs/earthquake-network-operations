7a766502d04d87f9ac0a8a79f4ad83cf0aef7e1c
ehunter-usgs <ehunter@usgs.gov>
2016-09-27 16:04:20 -0600

    Add getTelemetrys query to StationTelemetryFactory

--- src/lib/classes/StationTelemetryFactory.class.php
+++ src/lib/classes/StationTelemetryFactory.class.php
@@ -10,8 +10,26 @@ class StationTelemetryFactory {
     $this->pdo = $pdo;
   }
 
-  public function getTelemetrys() {
+  public function getTelemetrys($network = null, $station = null) {
+    $statement = $this->pdo->prepare(
+      'SELECT * ' .
+      'FROM netops_station ' .
+      'WHERE (network_code = :network OR :network is null) '.
+      'AND (station_code = :station OR :station is null)'
+    );
+    $statement->bindValue(':network', $network, PDO::PARAM_STR);
+    $statement->bindValue(':station', $station, PDO::PARAM_STR);
 
+    if ($statement->execute() === FALSE) {
+      // something went wrong
+      $errorInfo = $statement->errorInfo();
+      throw new Exception($errorInfo[2]);
+    } else {
+      $telemetrys = $statement->fetchAll(PDO::FETCH_ASSOC);
+      $statement->closeCursor();
+    }
+
+    return $telemetrys;
   }
 
 }
