682538a2629e68dccf200d0b9bc2d748d65b3857
Eric Martinez <emartinez@usgs.gov>
2016-10-04 08:50:55 -0600

    Added entrypoint service.php to htdocs. Very similar to example entrypoint.

---
+++ src/htdocs/service.php
@@ -0,0 +1,14 @@
+<?php
+
+  /* use configuration variables */
+  include_once '../conf/config.inc.php';
+  include_once '../lib/classes/StationTelemetryFactory.class.php';
+  include_once '../lib/classes/NetworkOperationsWebService.class.php';
+
+  $stf = new StationTelemetryFactory($DB);
+  $ws = new NetworkOperationsWebService($stf);
+  $obj = $_GET;
+
+  $ws->run($obj);
+
+?>
