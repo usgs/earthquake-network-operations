265fad7fbcf856136df109bbf4f60afa31450c1f
Eric Martinez <emartinez@usgs.gov>
2016-10-04 11:07:52 -0600

    Added documentation landing page.

---
+++ src/htdocs/telemetry.html.php
@@ -0,0 +1,11 @@
+<?php
+if (!isset($TEMPLATE)) {
+  $TITLE = 'Telemetry Webservice Documentation';
+
+  include_once 'template.inc.php';
+}
+?>
+
+<p class="alert error">
+  TODO :: Document this!
+</p>
