febed1b1eff6e873813be387a14db11295af5010
Eric Martinez <emartinez@usgs.gov>
2016-10-04 11:24:05 -0600

    Added rewrites for service/docs pages.

--- gruntconfig/connect.js
+++ gruntconfig/connect.js
@@ -8,6 +8,13 @@ var MOUNT_PATH = config.ini.MOUNT_PATH;
 
 var addMiddleware = function (connect, options, middlewares) {
   middlewares.unshift(
+    function (request, response, next) {
+      response.setHeader('Access-Control-Allow-Origin', '*');
+      response.setHeader('Access-Control-Allow-Methods', '*');
+      response.setHeader('Access-Control-Allow-Headers',
+          'accept,origin,authorization,content-type');
+      return next();
+    },
     require('grunt-connect-rewrite/lib/utils').rewriteRequest,
     require('grunt-connect-proxy/lib/utils').proxyRequest,
     require('gateway')(options.base[0], {
@@ -39,6 +46,14 @@ var connect = {
 
   rules: [
     {
+      from: '^' + MOUNT_PATH + '/telemetry.json(.*)$',
+      to: '/telemetry.json.php$1'
+    },
+    {
+      from: '^' + MOUNT_PATH + '/telemetry.html$',
+      to: '/telemetry.html.php'
+    },
+    {
       from: '^' + MOUNT_PATH + '/(.*)$',
       to: '/$1'
     }
@@ -52,7 +67,7 @@ var connect = {
       livereload: config.liveReloadPort,
       middleware: addMiddleware,
       open: 'http://localhost:' + config.buildPort +
-          MOUNT_PATH + '/index.php',
+          MOUNT_PATH + '/telemetry.html',
       port: config.buildPort
     }
   },
@@ -65,7 +80,7 @@ var connect = {
       port: config.distPort,
       keepalive: true,
       open: 'http://localhost:' + config.distPort +
-          MOUNT_PATH + '/index.php',
+          MOUNT_PATH + '/telemetry.html',
       middleware: addMiddleware
     }
   },
