d3169edb4b7d171dd42d2fbebfced9d390b2e53f
Eric Martinez <emartinez@usgs.gov>
2016-10-04 14:02:09 -0600

    Updated factory to manage its own connection based on parameters.

--- src/htdocs/telemetry.json.php
+++ src/htdocs/telemetry.json.php
@@ -5,7 +5,8 @@
   include_once '../lib/classes/StationTelemetryFactory.class.php';
   include_once '../lib/classes/NetworkOperationsWebService.class.php';
 
-  $stf = new StationTelemetryFactory($DB);
+  $stf = new StationTelemetryFactory($CONFIG['DB_DSN'],
+      $CONFIG['DB_USER'], $CONFIG['DB_PASS']);
   $ws = new NetworkOperationsWebService($stf);
   $obj = $_GET;
 
--- src/lib/classes/StationTelemetryFactory.class.php
+++ src/lib/classes/StationTelemetryFactory.class.php
@@ -4,34 +4,49 @@
 class StationTelemetryFactory {
 
   // the database connection to use
+  public $dsn;
+  public $pass;
   public $pdo;
   public $telemetryQuery;
+  public $user;
 
-  public function __construct($pdo = null) {
-    if (!$pdo) {
-      throw new Exception('PDO connection is not configured');
-    }
+  public function __construct($dsn, $user, $pass) {
+    $this->dsn = $dsn;
+    $this->user = $user;
+    $this->pass = $pass;
 
-    // set pdo connection
-    $this->pdo = $pdo;
+    $this->pdo = null;
     $this->telemetryQuery = null;
   }
 
   /**
-   * Get telemetry data.
+   * Initiates the connection to the database if not already done.
    *
-   * Returns all stations if no params are specified,
-   * otherwise only matching networks and stations are returned.
-   *
-   * @param network {String}
-   *      The network code
-   * @param station {String}
-   *      The station code
-   *
-   * @return {Array}
-   *      An array of telemetry data
+   * @return {StationTelemetryFactory}
+   *     A reference to this factory for method chaining purposes.
    */
-  public function getTelemetrys($network = null, $station = null) {
+  protected function connect () {
+    if ($this->pdo == null) {
+      try {
+        $this->pdo = new PDO($this->dsn, $this->user, $this->pass);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      } catch (Exception $ex) {
+        $handled = new Exception('Failed to connect to the database');
+        $handled->httpStatus = 500;
+        throw $handled;
+      }
+    }
+
+    return $this;
+  }
+
+  /**
+   * Initiates the statements to execute if not already done.
+   *
+   * @return {StationTelemetryFactory}
+   *     A reference to this factory for method chaining purposes.
+   */
+  protected function prepare () {
     if ($this->telemetryQuery == null) {
       // prepare statements
       $this->telemetryQuery = $this->pdo->prepare('
@@ -52,6 +67,28 @@ class StationTelemetryFactory {
       // set fetch mode
       $this->telemetryQuery->setFetchMode(PDO::FETCH_ASSOC);
     }
+
+    return $this;
+  }
+
+
+  /**
+   * Get telemetry data.
+   *
+   * Returns all stations if no params are specified,
+   * otherwise only matching networks and stations are returned.
+   *
+   * @param network {String}
+   *      The network code
+   * @param station {String}
+   *      The station code
+   *
+   * @return {Array}
+   *      An array of telemetry data
+   */
+  public function getTelemetrys($network = null, $station = null) {
+    // Make sure we are connected and ready to execute statements
+    $this->connect()->prepare();
 
     // bind values
     $this->telemetryQuery->bindValue(':network', $network, PDO::PARAM_STR);
